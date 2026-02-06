e5e3e339ad91d33e563df435803b50b4f2e50e77
Surendra26 <yanamadalasurendra@gmai.com>
2026-02-07 00:40:41 +0530

    Add root route /

--- app.py
+++ app.py
@@ -4,9 +4,12 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+@app.route("/")
+def home():
+    return "Welcome to my Flask app!"
+
 @app.route("/data")
 def get_data():
-    # BACKEND LOGIC ONLY
     name = "surendra"
     return jsonify({"message": f"Hi {name}"})
 
